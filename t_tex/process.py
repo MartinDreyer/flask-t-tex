98d7a7c4f7c88d19b04841ee0aafc9adbdf01b7d
Martin Dreyer <martinchristiandreyer@gmail.com>
2024-01-05 15:24:30 +0100

    dev refactor subprocess

--- t_tex/process.py
+++ t_tex/process.py
@@ -25,10 +25,32 @@ def allowed_file(filename):
 def optimize_file(filename):
     try:
         print("Converting file to .ogg")
-        command = '-i ' + filename + ' -vn -map_metadata -1 -ac 1 -c:a libopus -b:a 12k -application voip ' + filename.split(".")[0] + '.ogg'
-        ffmpeg_path = "ffmpeg" # In windows place ffmpeg.exe in t_tex: os.path.join(os.path.dirname(__file__), 'ffmpeg.exe')
-        cmd_string = ffmpeg_path + ' ' + command
-        subprocess.call(cmd_string, shell=True)
+        output_filename = filename.split(".")[0] + '.ogg'
+
+        ffmpeg_path = "ffmpeg"  # In Windows, provide the full path to ffmpeg.exe if not in the system PATH
+
+        # Construct the command as a list of arguments
+        command = [
+            ffmpeg_path,
+            '-i', filename,
+            '-vn',
+            '-map_metadata', '-1',
+            '-ac', '1',
+            '-c:a', 'libopus',
+            '-b:a', '12k',
+            '-application', 'voip',
+            output_filename
+        ]
+
+        # Run the command using subprocess.run()
+        result = subprocess.run(command, shell=False)
+
+        # Check the return code
+        if result.returncode == 0:
+            print(f"Conversion successful. Output file: {output_filename}")
+        else:
+            print(f"Error during conversion. Return code: {result.returncode}")
+            print(result.stderr)
     except Exception as e:
         print(f"Error: {e}")
     
@@ -80,31 +102,3 @@ def delete_output(filename):
                 print(f"Error deleting {file_path}: {e}")
     except Exception as e:
         print(f"Error: {e}")
-
-# @profile
-# def float_to_time(float_value: float):
-#     milliseconds = int((float_value % 1) * 1000)
-#     seconds = int(float_value % 60)
-#     minutes = int((float_value // 60) % 60)
-#     hours = int(float_value // 3600)
-
-#     time_str = f'{hours:02d}:{minutes:02d}:{seconds:02d},{milliseconds:03d}'
-#     return time_str
-
-# @profile
-# def output_to_text_file(data_dict: dict, output_file_name: str):
-#     index = 1
-#     try:
-#         with open(output_file_name, 'w', encoding='utf-8') as file:
-#             for value in data_dict['segments']:
-#                 start_time_str = float_to_time(value['start'])
-#                 end_time_str = float_to_time(value['end'])
-#                 text = value['text'].strip()
-#                 file.write(f'{index}\n')
-#                 file.write(f'{start_time_str} --> {end_time_str}\n')
-#                 file.write(f'{text}\n\n')
-#                 index += 1
-#             file.close()
-
-#     except Exception as e:
-#         print(f'Fejl ved skrivning til tekstfil: {e}')
